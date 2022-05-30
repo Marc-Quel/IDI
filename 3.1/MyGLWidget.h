8f4f5a3a266c96a5311e952fb077f8ca30879a4d
Marc <marcqueldev@gmail.com>
2022-05-30 19:23:48 +0000

    a

---
+++ 3.1/MyGLWidget.h
@@ -0,0 +1,25 @@
+// MyGLWidget.h
+#include "BL3GLWidget.h"
+
+class MyGLWidget : public BL3GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL3GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+    virtual void initializeGL ( );
+    virtual void mouseMoveEvent(QMouseEvent *e);
+    virtual void keyPressEvent(QKeyEvent* event);
+    virtual void iniMaterialTerra ();
+
+    void canviaPosFocus();
+    void canviaColFocus();
+    void canviaLlumAmbient();
+
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+    GLuint colFocusLoc, posFocusLoc, llumAmbientLoc;
+    float XFocus;
+};
